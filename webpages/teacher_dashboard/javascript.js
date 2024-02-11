220ee2060e58bdf284e5f4bcb2729cda26e88cc8
SKjustSK <sarthaksinghtariyal@gmail.com>
2024-02-11 17:50:17 +0530

    Added Menu bar functionality for teacher dashboard

--- webpages/teacher_dashboard/javascript.js
+++ webpages/teacher_dashboard/javascript.js
@@ -1,5 +1,7 @@
 // Menu button animation
 const menuButton = document.querySelector('.hamberger');
+const navigation = document.querySelector('.navigation');
 menuButton.addEventListener('click', () => {
     menuButton.classList.toggle('is-active');
+    navigation.classList.toggle('is-active');
 })
