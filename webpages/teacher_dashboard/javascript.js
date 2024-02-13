2e637b86f3a8dc2365f659d7a94720fa08602610
SKjustSK <sarthaksinghtariyal@gmail.com>
2024-02-14 02:08:20 +0530

    deleted redundant js file from eacher dashboard, slight changes to css (gap in lecture list page)

--- webpages/teacher_dashboard/javascript.js
+++
@@ -1,7 +0,0 @@
-// Menu button animation
-const menuButton = document.querySelector('.hamburger');
-const navigation = document.querySelector('.navigation');
-menuButton.addEventListener('click', () => {
-    menuButton.classList.toggle('is-active');
-    navigation.classList.toggle('is-active');
-})
